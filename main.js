91fe391ef018711fc6e9801658afa2f4443e50ce
Lautaro Zapata <oratualzc@gmail.com>
2023-10-06 04:10:54 -0300

    Fix Images

--- main.js
+++ main.js
@@ -21,51 +21,17 @@ const cargarCiudad = async (city) => {
     console.log(data);
 
     $cityName.innerHTML = data.name
-    $temperatura.innerHTML = Math.round(data.main.feels_like)+ ' °C'
+    $temperatura.innerHTML = Math.round(data.main.temp)+ ' °C'
     $desc_data.innerHTML = data.weather[0].main
-    
 
-    if (data.weather[0].main === 'overcast clouds' && data.weather[0].icon === '04n') {
-        $desc_icon.src = 'images/Clouds.png'
-        
-    } else if (data.weather[0].description === 'overcast clouds' && data.weather[0].icon === '04d'){
-        $desc_icon.src = 'images/ClearSkyDay.png'
-    }
-    else if (data.weather[0].description === 'clear sky' && data.weather[0].icon === '01d'){
-        $desc_icon.src = 'images/ClearSkyDay.png'
-    } else if (data.weather[0].description === 'clear sky' && data.weather[0].icon === '01n'){
-        $desc_icon.src = 'images/ClearSkyNight.png'
-    } else if (data.weather[0].description === 'few clouds' && data.weather[0].icon === '02n'){
-        $desc_icon.src = 'images/FewCloudsN.png'
-    }
-    else if (data.weather[0].description === 'few clouds' && data.weather[0].icon === '02d'){
-        $desc_icon.src = 'images/FewCloudsD.png'
-    } else if (data.weather[0].description === 'scattered clouds' && data.weather[0].icon === '03d'){
-        $desc_icon.src = 'images/ScatteredCloudsD.png'
-    } else if (data.weather[0].description === 'scattered clouds' && data.weather[0].icon === '03n'){
-        $desc_icon.src = 'images/ScatteredCloudsN.png'
-    } else if (data.weather[0].description === 'shower rain' && data.weather[0].icon === '09d'){
-        $desc_icon.src = 'images/ShowerRain.png'
-    }
-    else if (data.weather[0].description === 'shower rain' && data.weather[0].icon === '09n'){
-        $desc_icon.src = 'images/ShowerRain.png'
-    } else if (data.weather[0].description === 'rain' && data.weather[0].icon === '10d'){
-        $desc_icon.src = 'images/RainD.png'
-    } else if (data.weather[0].description === 'rain' && data.weather[0].icon === '10n'){
-        $desc_icon.src = 'images/RainN.png'
-    } else if (data.weather[0].description === 'thunderstorm' && data.weather[0].icon === '11d'){
-        $desc_icon.src = 'images/ThunderStorm.png'
-    } else if (data.weather[0].description === 'thunderstorm' && data.weather[0].icon === '11n'){
-        $desc_icon.src = 'images/ThunderStorm.png'
-    } else if (data.weather[0].description === 'snow' && data.weather[0].icon === '12d'){
-        $desc_icon.src = 'images/Snow.png'
-    } else if (data.weather[0].description === 'snow' && data.weather[0].icon === '12n'){
-        $desc_icon.src = 'images/Snow.png'
-    } else if (data.weather[0].description === 'mist' && data.weather[0].icon === '50d'){
-        $desc_icon.src = 'images/Mist.png'
-    } else if (data.weather[0].description === 'mist' && data.weather[0].icon === '50n'){
-        $desc_icon.src = 'images/Mist.png'
-    }
+
+    $desc_icon.setAttribute('src',`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+    console.log($desc_icon);
+
+
+
+
+    
 }
 
 $submit.addEventListener('click',(e)=>{
